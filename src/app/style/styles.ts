816cf1daeef44ef03b6c284b2865f0f54efcb6ab
Brent Van Steertegem <brent.vansteertegem@gmail.com>
2024-08-01 10:07:35 +0200

    Initial commit

---
+++ src/app/style/styles.ts
@@ -0,0 +1,36 @@
+const Breakpoints = {
+    tablet: '36rem'
+}
+  
+const Colors = {
+    neutrals: {
+        white: '#FFF',
+        black: '#000',
+    }
+}
+  
+const TextColors = {
+    light: Colors.neutrals.white,
+    dark: Colors.neutrals.black
+}
+  
+const Sizes = {
+    medium: '1rem',
+}
+  
+const Padding = {
+    medium: Sizes.medium
+}
+  
+const DefaultStyles = {
+    padding: Padding.medium,
+}
+  
+export const Variables = {
+    breakpoints: Breakpoints,
+    colors: Colors,
+    textColors: TextColors,
+    sizes: Sizes,
+    padding: Padding,
+    defaultStyles: DefaultStyles
+}
