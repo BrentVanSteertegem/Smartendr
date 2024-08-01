e867e2bc9011e302ae032fc302455adab0a21000
Brent Van Steertegem <brent.vansteertegem@gmail.com>
2024-08-01 10:19:25 +0200

    feat: Fetch orders on page load

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const App = () => {
+    const [loading, setLoading] = useState(true)
+    const [orders, setOrders] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await fetch('https://staging.smartendr.be/app/api_get_orders?locations=23,12&timestamp=43399')
+        return await data.json()
+        }
+
+        fetchData().then(data => {
+            data.orders && setOrders(data.orders)
+            console.log(data.orders)
+            setLoading(false)
+        }).catch(e => {
+            // handle error
+            console.error(e)
+            setLoading(false)
+        })
+    }, [])
+
     return (
-        <h1>Hello Smartendr!</h1>
+        <h1>{loading ? "Loading..." : "Hello Smartendr!"}</h1>
     )
 }
 
-export default App;
+export default App
