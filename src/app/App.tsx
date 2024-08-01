1ea9d5ca404376967da0085f3ebb67500dc72319
Brent Van Steertegem <brent.vansteertegem@gmail.com>
2024-08-01 11:00:00 +0200

    chore: Exported data fetching to external function

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,11 @@
 import React, { useState, useEffect } from 'react'
+import { fetchData } from './data'
 
 const App = () => {
     const [loading, setLoading] = useState(true)
     const [orders, setOrders] = useState([])
 
     useEffect(() => {
-        const fetchData = async () => {
-            const data = await fetch('https://staging.smartendr.be/app/api_get_orders?locations=23,12&timestamp=43399')
-        return await data.json()
-        }
-
         fetchData().then(data => {
             data.orders && setOrders(data.orders)
             console.log(data.orders)
